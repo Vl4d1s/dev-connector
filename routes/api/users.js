9658a532f6b312de151187194dc6ec7b8dd03a81
Vladis Markin <vladismarkin@gmail.com>
2020-10-10 18:16:56 +0300

    Request & Body Validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const router = express.Router();
 
-// @route   GET api/useres
-// @desc    Test route
+// @route   POST api/useres
+// @desc    Register user
 // @access  Public
-router.get('/',(req,res)=>{res.send('User route')});
+router.post('/',(req,res)=>{
+    console.log(req.body)
+    res.send('User route')
+
+
+});
 
 module.exports = router;
