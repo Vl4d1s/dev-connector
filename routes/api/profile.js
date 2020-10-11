7ef34fb2af855e1f704b90434d3a2dd74c14e45d
Vladis Markin <vladismarkin@gmail.com>
2020-10-11 20:42:49 +0300

    Adding prettier dev depend...

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,11 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
 
 // @route   GET api/profile
 // @desc    Test route
 // @access  Public
-router.get('/',(req,res)=>{res.send('Profile route')});
+router.get('/', (req, res) => {
+  res.send('Profile route');
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 connectDB();
 
 // Init Middleware
-app.use(express.json({extended: false}))
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('API Running');
@@ -18,7 +18,6 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
-
 
 // Serve static assets in production
 
